c465029eebd0228b2bf212376d9dc2ef7740f1a5
Jason Pua <jasonpua@Jasons-MacBook-Air.local>
2024-10-02 16:57:43 +0800

    first commit

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -17,7 +17,7 @@ export default function Chat() {
         <input
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
-          placeholder="Please ask me a question about my resume"
+          placeholder="Say something..."
           onChange={handleInputChange}
         />
       </form>
